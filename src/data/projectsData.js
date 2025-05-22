0f97ba4d47a57fd13a5c546b98c969f821daaed5
OreoIsCoding <axeldionisio0103@gmail.com>
2025-05-22 10:55:14 +0800

    Updated projects data

--- src/data/projectsData.js
+++ src/data/projectsData.js
@@ -2,14 +2,16 @@ import myPortfolio from "../assets/project-images/my-portfolio.jpg";
 import enWeb from "../assets/project-images/snacf.jpg";
 import quizBee from "../assets/project-images/QuizBee.jpg";
 import typingTest from "../assets/project-images/typing_test.jpg";
+import cao from "../assets/project-images/cao.jpg";
 import projectsDataJson from '../../datasets/projectsData.json';
-
+ 
 // Map image filenames to imported images
 const imageMap = {
   "my-portfolio.jpg": myPortfolio,
   "snacf.jpg": enWeb,
   "QuizBee.jpg": quizBee,
-  "typing_test.jpg": typingTest
+  "typing_test.jpg": typingTest,
+  "cao.jpg": cao,
 };
 
 export const projectsData = projectsDataJson.map(project => ({
